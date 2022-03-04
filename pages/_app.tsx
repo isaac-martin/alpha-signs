22c031a0b96c6064b081d71065f7dcd5674bd413
Isaac Martin <hey@isaacmartin.co>
2022-03-04 12:06:51 +1100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0';
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
-export default MyApp
+export default App
+
